0c0ac3026b13312549d6424a51dd3dcb104aa31b
Etudiant <etudiant@example.com>
2026-04-13 15:13:41 -0400

    Changement du message de QA

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
   dev: "Hello de l'environnement de DEV",
-  qa: "Hello de l'environnement de QA",
+  qa: "Bonjour  de l'environnement de QA: It Is A Test",
   prod: "Hello de l'environnement de PROD",
 };
 
