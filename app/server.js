21a492aac22b8a2524bb29ee29aba071b6304db0
Etudiant <etudiant@example.com>
2026-04-24 17:41:45 -0400

    test avant remise

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Bonjour de l'environnement de DEV Test v1"
+  dev: "Bonjour de l'environnement de DEV Test v2"
   
 };
 
